30084f0fece73e0b76db0c4b664b54abdfb458f0
erdaserhan <erhan209@gmail.com>
2024-10-28 14:00:51 +0100

    security controller ok, adminController created

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SectionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(SectionRepository $sectionRepository): Response
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'sections' => $sectionRepository->findAll(),
         ]);
     }
 }
