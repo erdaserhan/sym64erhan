ca704c4710a5dea22b60f34c8d6f2e60d2940c63
erdaserhan <erhan209@gmail.com>
2024-10-28 15:25:21 +0100

    dashbord OK, MainController updated(sections, articles)

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,19 +2,27 @@
 
 namespace App\Controller;
 
-use App\Repository\SectionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Article;
+use App\Entity\Section;
+
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(SectionRepository $sectionRepository): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $sections = $em->getRepository(Section::class)->findAll();
+        $articles = $em->getRepository(Article::class)->findBy(['published'=>true], ['articleDatePosted' => 'DESC'], 10);
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-            'sections' => $sectionRepository->findAll(),
+            // on envoie les catégories à la vue
+            'sections' => $sections,
+            // on envoie les articles à la vue
+            'articles' => $articles,
         ]);
     }
+
 }
