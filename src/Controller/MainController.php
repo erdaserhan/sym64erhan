ade5598f4c90405a5396317bd14e7e931553bc19
erdaserhan <erhan209@gmail.com>
2024-10-28 15:37:39 +0100

    section with slug ok on mainController

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -18,9 +18,7 @@ class MainController extends AbstractController
         $sections = $em->getRepository(Section::class)->findAll();
         $articles = $em->getRepository(Article::class)->findBy(['published' => true], ['articleDatePosted' => 'DESC'], 10);
         return $this->render('main/index.html.twig', [
-            // on envoie les catégories à la vue
             'sections' => $sections,
-            // on envoie les articles à la vue
             'articles' => $articles,
         ]);
     }
