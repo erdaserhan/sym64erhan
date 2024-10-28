65e6464c119cd0913901bbcb837b371df0a6f790
erdaserhan <erhan209@gmail.com>
2024-10-28 16:09:31 +0100

    importmap css updated

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,12 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
-import './vendor/bootstrap/dist/css/bootstrap.min.css';
+
+
+// import './styles/app.css';
+
+import './styles/style.css';
+import './styles/bootstrap.min.css';
+// import './vendor/bootstrap/dist/css/bootstrap.min.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
